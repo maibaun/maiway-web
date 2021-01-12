755d1d55258475a33643711d0e51d4cc9261cdcb
celmar <celmargalindez@gmail.com>
2021-01-12 10:10:35 +0800

    january 2021

---
+++ gh_pages.js
@@ -0,0 +1,15 @@
+var ghpages = require("gh-pages");
+ghpages.publish(
+  "build",
+  {
+    branch: "master",
+    repo: "https://github.com/maiway-web/maiway-web.github.io.git",
+  },
+  (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("success");
+    }
+  }
+);
